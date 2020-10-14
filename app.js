d9334cdfaa69c6d5a993edddd5779d3bea0bb052
Saron <saronnhong@gmail.com>
2020-10-13 18:18:26 -0700

    all this

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv/config');
+
+
+//Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+//Import Routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
+
+//Routes
+app.get('/', (req, res) => {
+    res.send("We are on home");
+})
+
+//Connect to database
+mongoose.connect(
+    process.env.DB_CONNECTION, 
+    { useNewUrlParser: true, useUnifiedTopology: true }, 
+    () => console.log('Connected to DB')
+)
+
+//Start to listen to the server
+app.listen(3000);
